ea8810b1c5156e86b63a6a6c54efa7a0d2ac30d4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-09 08:37:24 -0400

    verbatim

--- S25W1CSharpFundamentals/Program.cs
+++ S25W1CSharpFundamentals/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @double = 5;
+
+            int i = 5;
+            double d = 5.5;
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 5L;
+
+            var x = 5.5;
+
+
+            Console.WriteLine(@"hello\nworld");
         }
     }
 }
