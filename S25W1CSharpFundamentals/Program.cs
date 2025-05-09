f00d98deebc94875053be8be86e97ee370f01141
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-05-09 08:16:26 -0400

    Add project files.

---
+++ S25W1CSharpFundamentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S25W1CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
